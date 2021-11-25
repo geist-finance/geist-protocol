fcc270e795044d97b18a2a448560297ffb677f32
aaveenhuijari <aaveenhuijari@protonmail.com>
2021-11-25 17:34:35 +0400

    update addresses and function names for use with geist

--- contracts/staking/ProtocolOwnedDEXLiquidity.sol
+++ contracts/staking/ProtocolOwnedDEXLiquidity.sol
@@ -4,7 +4,7 @@ import "../dependencies/openzeppelin/contracts/SafeMath.sol";
 import "../dependencies/openzeppelin/contracts/IERC20.sol";
 import "../interfaces/IChefIncentivesController.sol";
 
-interface IJoePair {
+interface IUniswapLPToken {
     function getReserves()
         external
         view
@@ -27,47 +27,47 @@ contract ProtocolOwnedDEXLiquidity {
 
     using SafeMath for uint256;
 
-    IJoePair constant public lpToken = IJoePair(0xac3F978714c613E768272c502a8912bC03DCf624);
-    IERC20 constant public bAVAX = IERC20(0xB2AC04b71888E17Aa2c5102cf3d0215467D74100);
-    address constant public treasury = 0xA867c1acA4B5F1E0a66cf7b1FE33525D57608854;
+    IUniswapLPToken constant public lpToken = IUniswapLPToken(0x668AE94D0870230AC007a01B471D02b2c94DDcB9);
+    IERC20 constant public gFTM = IERC20(0x39B3bd37208CBaDE74D0fcBDBb12D606295b430a);
+    address constant public treasury = 0x49c93a95dbcc9A6A4D8f77E59c038ce5020e82f8;
 
-    uint public totalSoldAVAX;
+    uint public totalSoldFTM;
 
-    event SoldAVAX(
+    event SoldFTM(
         address indexed buyer,
         uint256 amount
     );
 
     constructor() {
-        IChefIncentivesController chef = IChefIncentivesController(0x2d867AE30400ffFaD9BeD8472c514c2d6b827F5f);
+        IChefIncentivesController chef = IChefIncentivesController(0x297FddC5c33Ef988dd03bd13e162aE084ea1fE57);
         chef.setClaimReceiver(address(this), treasury);
     }
 
-    function protocolOwnedReserves() public view returns (uint256 blizz, uint256 wavax) {
+    function protocolOwnedReserves() public view returns (uint256 wftm, uint256 geist) {
         (uint reserve0, uint reserve1,) = lpToken.getReserves();
         uint balance = lpToken.balanceOf(address(this));
         uint totalSupply = lpToken.totalSupply();
         return (reserve0.mul(balance).div(totalSupply), reserve1.mul(balance).div(totalSupply));
     }
 
-    function availableAVAX() public view returns (uint256) {
-        return bAVAX.balanceOf(address(this)) / 2;
+    function availableFTM() public view returns (uint256) {
+        return gFTM.balanceOf(address(this)) / 2;
     }
 
-    function lpTokensPerOneAVAX() public view returns (uint256) {
+    function lpTokensPerOneFTM() public view returns (uint256) {
         uint totalSupply = lpToken.totalSupply();
         (,uint reserve1,) = lpToken.getReserves();
         return totalSupply.mul(1e18).mul(45).div(reserve1).div(100);
     }
 
-    function buyAVAX(uint256 amount) public {
-        require(amount >= 1e18, "Must purchase at least 1 WAVAX");
-        uint lpAmount = amount.mul(lpTokensPerOneAVAX()).div(1e18);
+    function buyFTM(uint256 amount) public {
+        require(amount >= 1e18, "Must purchase at least 1 WFTM");
+        uint lpAmount = amount.mul(lpTokensPerOneFTM()).div(1e18);
         lpToken.transferFrom(msg.sender, address(this), lpAmount);
-        bAVAX.transfer(msg.sender, amount);
-        bAVAX.transfer(treasury, amount);
-        totalSoldAVAX = totalSoldAVAX.add(amount);
-        emit SoldAVAX(msg.sender, amount);
+        gFTM.transfer(msg.sender, amount);
+        gFTM.transfer(treasury, amount);
+        totalSoldFTM = totalSoldFTM.add(amount);
+        emit SoldFTM(msg.sender, amount);
     }
 }
 
